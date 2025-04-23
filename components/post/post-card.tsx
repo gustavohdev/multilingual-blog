8dc10505c42424df232d2987f922fa2c10f14218
Gustavo Avide <g_a93@outlook.com>
2025-04-23 17:49:11 +0100

    fix: image height affecting overlapping footer layout

--- components/post/post-card.tsx
+++ components/post/post-card.tsx
@@ -17,21 +17,23 @@ const PostCard = ({
   locale,
 }: PostProps) => {
   return (
-    <Link
-      className={`@container ${layout === 'horizontal' ? 'grid grid-cols-1 md:grid-cols-2 gap-10' : 'space-y-10'}`}
-      href={`/${locale}/post/${post.slug}`}
-    >
-      {/* Post Image  */}
-      <Image
-        className={`rounded-md w-full object-cover object-center h-full max-h-[300px] ${reverse ? 'md:order-last' : ''}`}
-        alt={post.title!}
-        src={`${process.env.NEXT_PUBLIC_ASSETS_URL}/${post.image}?key=optimised`}
-        width={600}
-        height={300}
-      />
-      {/* Post Content */}
-      <PostContent locale={locale} post={post} />
-    </Link>
+    <div>
+      <Link
+        className={`@container ${layout === 'horizontal' ? 'grid grid-cols-1 md:grid-cols-2 gap-10' : 'space-y-10'}`}
+        href={`/${locale}/post/${post.slug}`}
+      >
+        {/* Post Image  */}
+        <Image
+          className={`rounded-md w-full object-cover object-center h-[250px] ${reverse ? 'md:order-last' : ''}`}
+          alt={post.title!}
+          src={`${process.env.NEXT_PUBLIC_ASSETS_URL}/${post.image}?key=optimised`}
+          width={600}
+          height={300}
+        />
+        {/* Post Content */}
+        <PostContent locale={locale} post={post} />
+      </Link>
+    </div>
   );
 };
 
